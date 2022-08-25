bf215d0894bb39cb012b5c8ea9f7f3447b574b8d
KubaTworek <kubatvorek@gmail.com>
2022-08-25 22:45:20 +0200

    First Config

---
+++ Restaurant-Management-System/src/test/java/pl/jakubtworek/RestaurantManagementSystem/RestaurantManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakubtworek.RestaurantManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
