4eb295541374b41fc9f57aee5a32ee66d4b4ef84
KubaTworek <kubatvorek@gmail.com>
2022-12-08 00:36:01 +0100

    add spring security

---
+++ src/main/java/pl/jakubtworek/RestaurantManagementSystem/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package pl.jakubtworek.RestaurantManagementSystem.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
+    }
+
+}
