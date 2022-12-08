99dc1f963577a1a2f7439222e45854d3067e2af9
KubaTworek <kubatvorek@gmail.com>
2022-12-08 22:16:39 +0100

    add cutom authentication provider

---
+++ src/main/java/pl/jakubtworek/RestaurantManagementSystem/config/RestaurantAuthenticationProvider.java
@@ -0,0 +1,38 @@
+package pl.jakubtworek.RestaurantManagementSystem.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import pl.jakubtworek.RestaurantManagementSystem.model.entity.User;
+import pl.jakubtworek.RestaurantManagementSystem.repository.UserRepository;
+
+import java.util.*;
+
+@Component
+@RequiredArgsConstructor
+public class RestaurantAuthenticationProvider implements AuthenticationProvider{
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new BadCredentialsException("No user registered with this username!"));
+        if (passwordEncoder.matches(password, user.getPassword())) {
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(user.getAuthorities().getAuthority()));
+            return new UsernamePasswordAuthenticationToken(username, password, authorities);
+        } else {
+            throw new BadCredentialsException("Invalid password!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authenticationType) {
+        return authenticationType.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
--- src/main/java/pl/jakubtworek/RestaurantManagementSystem/service/impl/UserServiceImpl.java
+++ src/main/java/pl/jakubtworek/RestaurantManagementSystem/service/impl/UserServiceImpl.java
@@ -1,10 +1,8 @@
 package pl.jakubtworek.RestaurantManagementSystem.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 import pl.jakubtworek.RestaurantManagementSystem.model.dto.UserDTO;
-import pl.jakubtworek.RestaurantManagementSystem.model.entity.*;
 import pl.jakubtworek.RestaurantManagementSystem.model.entity.User;
 import pl.jakubtworek.RestaurantManagementSystem.repository.UserRepository;
 import pl.jakubtworek.RestaurantManagementSystem.service.UserService;
@@ -13,16 +11,9 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserDetailsService, UserService {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User details not found for the user: " + username));
-        return new SecurityUser(user);
-    }
 
     @Override
     public Optional<UserDTO> findByUsername(String username) {
