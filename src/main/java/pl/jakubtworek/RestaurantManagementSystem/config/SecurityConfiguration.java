81eec8e58299aa263063a920a35bb7f869062651
KubaTworek <kubatvorek@gmail.com>
2022-12-12 02:25:04 +0100

    update security config

--- src/main/java/pl/jakubtworek/RestaurantManagementSystem/config/SecurityConfiguration.java
+++ src/main/java/pl/jakubtworek/RestaurantManagementSystem/config/SecurityConfiguration.java
@@ -3,8 +3,12 @@ package pl.jakubtworek.RestaurantManagementSystem.config;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,9 +18,18 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeHttpRequests((authz) -> authz
-                      //  .anyRequest().authenticated()
+                .cors().configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:9090"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L);
+                    return config;
+                })
+                .and().csrf().ignoringAntMatchers("/contact")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and().authorizeHttpRequests((authz) -> authz
                         .antMatchers("/orders").hasAuthority("admin")
                         .antMatchers("/employees").hasAuthority("user")
                         .antMatchers("/menu", "menu-items", "/register").permitAll()
